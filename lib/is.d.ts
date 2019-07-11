d62f62ffc45c84caa2a18f681a0241253a8144f0
Tatu Anttonen <tatu.anttonen@gmail.com>
2019-07-11 19:24:09 +0300

    Removed lib folder

--- lib/is.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const isDefined: (val: any) => boolean;
-export declare const isUndefined: (val: any) => val is undefined;
-export declare const isBoolean: (val: any) => val is boolean;
-export declare const isObject: (val: any) => val is object;
-export declare const isArray: (val: any) => val is any[];
-export declare const isNumber: (val: any) => val is number;
-export declare const isString: (val: any) => val is string;
--- lib/is.ts
+++
@@ -1,8 +0,0 @@
-export const isDefined = (val: any) => typeof val !== 'undefined';
-export const isUndefined = (val: any): val is undefined => typeof val === 'undefined';
-export const isBoolean = (val: any): val is boolean => typeof val === 'boolean';
-export const isObject = (val: any): val is object => typeof val === 'object';
-export const isArray = (val: any): val is Array<any> => typeof val === 'object' && Array.isArray(val);
-export const isNumber = (val: any): val is number =>
-	(typeof val === 'number' && isFinite(val)) || (val !== '' && isFinite(Number(val)))
-export const isString = (val: any): val is string => typeof val === 'string';
